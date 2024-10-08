85ebfce905642efe574af6ac2cb1791f5bfd9d81
Primawira <muhamadprimawira@gmail.com>
2024-10-08 12:48:26 +0700

    p

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import express from "express"
-import "dotenv/config"
-import "./model/index.js"
-import router from "./routes/route.js"
-const app = express()
-const PORT = process.env.PORT
-
+import express from "express";
+import "dotenv/config";
+import "./model/index.js";
+import router from "./routes/route.js";
+import cors from "cors";
 import bodyParser from "body-parser";
-app.use(express.json());
-app.use(bodyParser.json())
-  app.use("/", router)
 
-app.listen(PORT,() => {
-    console.log("server running at http://localhost:" + PORT)
- })
+const app = express();
+const PORT = process.env.PORT;
+
+// Pasang CORS sebelum router
+app.use(cors()); 
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log("server running at http://localhost:" + PORT);
+});
